c3e862d1c2fb3070df8cd6d493223121f1175b61
Céline <celinesou77@gmail.com>
2024-07-31 13:23:40 +0200

    correction balise img en Image & description transformé en string

--- src/components/MyInfo.tsx
+++ src/components/MyInfo.tsx
@@ -8,22 +8,8 @@ export default function MyInfo() {
 			</div>
 			<div className="p-4 border-t border-base-content bg-base-100">
 				<h2 className="mt-2">Mon parcours</h2>
-				<p>
-					Passionnée par les métiers du numérique, j'ai suivi une formation
-					de développeur web chez OpenClassrooms, où j'ai développé une
-					solide base technique et acquis des compétences essentielles pour
-					le développement web. Rigoureuse et curieuse, je suis constamment
-					en quête de nouvelles connaissances et de perfectionnement.
-					<br />
-					<br />
-					Actuellement en formation à Ada Tech School, je me spécialise en
-					tant que développeuse web Full Stack. Je suis à la recherche
-					d'une entreprise pour m'accueillir en alternance à partir de
-					novembre 2024 ou janvier 2025, pour une durée d'un an, avec un
-					rythme de 4 jours en entreprise et 1 jour en formation. Mon
-					objectif est de mettre mes compétences au service de projets
-					innovants tout en continuant à développer mon expertise dans un
-					environnement stimulant.
+				<p className=" whitespace-pre-line">
+					{"Passionnée par les métiers du numérique, j'ai suivi une formation de développeur web chez OpenClassrooms, où j'ai développé une solide base technique et acquis des compétences essentielles pour le développement web. Rigoureuse et curieuse, je suis constamment en quête de nouvelles connaissances et de perfectionnement.\n\nActuellement en formation à Ada Tech School, je me spécialise en tant que développeuse web Full Stack. Je suis à la recherche d'une entreprise pour m'accueillir en alternance à partir de novembre 2024 ou janvier 2025, pour une durée d'un an, avec un rythme de 4 jours en entreprise et 1 jour en formation. Mon objectif est de mettre mes compétences au service de projets innovants tout en continuant à développer mon expertise dans un environnement stimulant."}
 				</p>
 			</div>
 		</div>
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,128 +1,126 @@
 "use client";
 import { Project } from "@/assets/projectsData";
+import Image from "next/image";
 
 interface OneProject extends Project {
-    modalId: string;
+	modalId: string;
 }
 
 export default function ProjectCard(props: OneProject) {
-    const { title, description, githubLink, webLink, stack, modalId, images } =
-        props;
+	const { title, description, githubLink, webLink, stack, modalId, images } =
+		props;
 
-    return (
-        <li className="p-0 m-0">
-            <div className="mockup-window border border-base-content bg-secondary basis-full md:basis-5/12 text-secondary-content">
-                <div className="p-2 bg-base-100 h-fit flex flex-col justify-between text-base-content border-t border-base-content">
-                    <div className="h-24 flex justify-center items-center">
-                        <h2 className="my-0 text-center">{title}</h2>
-                    </div>
+	return (
+		<li className="p-0 m-0">
+			<div className="mockup-window border border-base-content bg-secondary basis-full md:basis-5/12 text-secondary-content">
+				<div className="p-2 bg-base-100 h-fit flex flex-col justify-between text-base-content border-t border-base-content">
+					<div className="h-24 flex justify-center items-center">
+						<h2 className="my-0 text-center">{title}</h2>
+					</div>
 
-                    <div className="divider my-0"></div>
+					<div className="divider my-0"></div>
 
-                    <div className="h-64 flex justify-center items-center">
-                        {images.length === 0 ? (
-                            <div className="text-wrap text-center">
-                                <p>{stack.join(" | ")}</p>
-                            </div>
-                        ) : (
-                            <img
-                                className="max-h-full m-0 object-contain"
-                                src={images[0].url}
-                                alt=""
-                            />
-                        )}
-                    </div>
-                    <div className="self-end m-3">
-                        <button
-                            onClick={() => handleShowModal(modalId)}
-                            className="btn btn-accent sm:btn-wide"
-                        >
-                            + Détails
-                        </button>
-                    </div>
-                    <ProjectModal key={modalId} {...props} />
-                </div>
-            </div>
-        </li>
-    );
+					<div className="h-64 flex justify-center items-center relative">
+						{images.length === 0 ? (
+							<div className="text-wrap text-center">
+								<p>{stack.join(" | ")}</p>
+							</div>
+						) : (
+							<Image
+								className=" object-contain m-0"
+								src={images[0].url}
+								alt=""
+								loading="lazy"
+								fill
+							/>
+						)}
+					</div>
+					<div className="self-end m-3">
+						<button
+							onClick={() => handleShowModal(modalId)}
+							className="btn btn-accent sm:btn-wide"
+						>
+							+ Détails
+						</button>
+					</div>
+					<ProjectModal key={modalId} {...props} />
+				</div>
+			</div>
+		</li>
+	);
 }
 
 function handleShowModal(id: string) {
-    if (document) {
-        (document.getElementById(id) as HTMLFormElement).showModal();
-    }
+	if (document) {
+		(document.getElementById(id) as HTMLFormElement).showModal();
+	}
 }
 
 function ProjectModal({
-    title,
-    description,
-    githubLink,
-    webLink,
-    stack,
-    modalId,
-    images,
+	title,
+	description,
+	githubLink,
+	webLink,
+	stack,
+	modalId,
+	images,
 }: OneProject) {
-    let splitDescription = description.split("\n");
-    return (
-        <dialog id={modalId} className="modal modal-bottom sm:modal-middle">
-            <div className="modal-box sm:w-10/12 sm:max-w-5xl">
-                <form method="dialog">
-                    <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-                        ✕
-                    </button>
-                </form>
-                <h3 className="font-bold text-lg">{title}</h3>
-                <p>{stack.join(" | ")}</p>
-                {splitDescription.map((paragraph, i) => {
-                    return <p key={`paragraph${i}`}>{paragraph}</p>;
-                })}
-                <div className="flex flex-wrap gap-5">
-                    <a
-                        className="link link-info"
-                        target="_blank"
-                        href={githubLink}
-                    >
-                        Lien GitHub
-                    </a>
-                    {webLink.length > 0 ? (
-                        <a
-                            className="link link-info"
-                            href={webLink}
-                            target="_blank"
-                        >
-                            Lien site web
-                        </a>
-                    ) : null}
-                </div>
-                {images.length != 0 ? (
-                    <div className="flex flex-col justify-between">
-                        {images.map((img, i) => {
-                            return (
-                                <div key={`projectImg-${i}`} className="my-4 flex flex-col">
-                                    {i != 0 ? (
-                                        <div className="divider"></div>
-                                    ) : null}
-                                    <h4>{img.alt}</h4>
-                                    <a
-                                        href={img.url}
-                                        target="_blank"
-                                        className="block h-80 self-center"
-                                    >
-                                        <img
-                                            className="mt-0 h-full object-contain"
-                                            src={img.url}
-                                            alt={img.alt}
-                                        />
-                                    </a>
-                                </div>
-                            );
-                        })}
-                    </div>
-                ) : null}
-            </div>
-            <form method="dialog" className="modal-backdrop">
-                <button>close</button>
-            </form>
-        </dialog>
-    );
+	let splitDescription = description.split("\n");
+	return (
+		<dialog id={modalId} className="modal modal-bottom sm:modal-middle">
+			<div className="modal-box sm:w-10/12 sm:max-w-5xl">
+				<form method="dialog">
+					<button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+						✕
+					</button>
+				</form>
+				<h3 className="font-bold text-lg">{title}</h3>
+				<p>{stack.join(" | ")}</p>
+				{splitDescription.map((paragraph, i) => {
+					return <p key={`paragraph${i}`}>{paragraph}</p>;
+				})}
+				<div className="flex flex-wrap gap-5">
+					<a className="link link-info" target="_blank" href={githubLink}>
+						Lien GitHub
+					</a>
+					{webLink.length > 0 ? (
+						<a className="link link-info" href={webLink} target="_blank">
+							Lien site web
+						</a>
+					) : null}
+				</div>
+				{images.length != 0 ? (
+					<div className="flex flex-col justify-between">
+						{images.map((img, i) => {
+							return (
+								<div
+									key={`projectImg-${i}`}
+									className="my-4 flex flex-col"
+								>
+									{i != 0 ? <div className="divider"></div> : null}
+									<h4>{img.alt}</h4>
+									<a
+										href={img.url}
+										target="_blank"
+										className="block h-80 w-full self-center relative"
+									>
+										<Image
+											className="object-contain mt-0"
+											src={img.url}
+											alt={img.alt}
+											loading="lazy"
+											fill
+										/>
+									</a>
+								</div>
+							);
+						})}
+					</div>
+				) : null}
+			</div>
+			<form method="dialog" className="modal-backdrop">
+				<button>close</button>
+			</form>
+		</dialog>
+	);
 }
